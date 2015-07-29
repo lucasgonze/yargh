dd2c36c053436d37140f35f5f309e5dc84972e21
Lucas Gonze <lucas@gonze.com>
2015-07-29 09:02:41 -0700

    Refactor CLI UX described in readme. Rewrite readme. Start new code in ^_^.js.

---
+++ ^_^.js
@@ -0,0 +1,51 @@
+
+/*
+ * Work in progress - messy workbench state, just working out my thoughts.
+ *
+ * c. 2015 Lucas Gonze
+ * MIT license
+ */
+
+var program = require('commander');
+
+var cmd;
+var sep;
+
+program
+  .version('0.0.1')
+  .arguments('<command-template> [separator]')
+  .action(function (cmdParam, sepParam) {
+     cmd = cmdParam;
+     sep = sepParam;
+  });
+
+program.parse(process.argv);
+
+if (typeof cmd === 'undefined') {
+   console.error('no command given!');
+   process.exit(1);
+}
+console.log('command:', cmd);
+console.log('separator:', sep || "no separator given");
+
+//*****************
+
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin,
+  terminal: false
+});
+
+var sys = require('sys')
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { console.log(stdout) };
+
+rl.on('line', function(line){
+
+    // first case: neither separator nor regex
+    // use incoming line as replacement for $0 in replace str
+    var newStr = cmd.replace("%0", line);
+
+    exec(newStr, puts);
+
+})
